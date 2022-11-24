5780d43e1fab909d19d11baa94746da7dcdd0dfa
PeaceOloruntoba <profprincepeace@gmail.com>
2022-11-24 03:12:50 +0100

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <title>Klassy Cafe - Restaurant HTML Template</title>
-<!--
-    
-TemplateMo 558 Klassy Cafe
 
-https://templatemo.com/tm-558-klassy-cafe
-
--->
     <!-- Additional CSS Files -->
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 
